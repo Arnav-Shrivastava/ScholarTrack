29260ac1d4f4ad8234faa963de9b8cca5eb1f104
Arnav-Shrivastava <arnavshrivastava01@gmail.com>
2026-01-17 01:53:20 +0530

    configure alembic and add initial migration

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,5 +1,8 @@
 from logging.config import fileConfig
 
+from app.core.config import settings
+from app.database.base import Base
+
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
@@ -18,7 +21,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -58,7 +62,9 @@ def run_migrations_online() -> None:
 
     """
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        {
+            "sqlalchemy.url": settings.DATABASE_URL
+        },
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
