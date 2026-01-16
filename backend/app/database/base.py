719f8adc6f6ae3d7d34b65027c7706209e09f416
Arnav-Shrivastava <arnavshrivastava01@gmail.com>
2026-01-17 01:47:24 +0530

    add user model and users table

--- backend/app/database/base.py
+++ backend/app/database/base.py
@@ -1,3 +1,6 @@
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
+
+# Import models here so Alembic can detect them later
+from app.models.user import User
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 from app.api.v1.health import router as health_router
+from app.database.session import engine
+from app.database.base import Base
+
 
 app = FastAPI(
     title="ScholarTrack API",
     version="1.0.0"
 )
+
+Base.metadata.create_all(bind=engine)
+
 
 app.include_router(health_router, prefix="/api/v1")
 
